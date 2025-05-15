f8a41aee6b7fc7d4a70d4550142c4c131daf34c6
thendarion <16006640+thendarion@users.noreply.github.com>
2025-05-15 06:58:14 +0200

    try a separate upload approach
    Signed-off-by: thendarion <16006640+thendarion@users.noreply.github.com>

--- palindrome/tasks.py
+++ palindrome/tasks.py
@@ -3,11 +3,11 @@ from invoke import task
 
 @task
 def test_robot(c):
-    c.run("robot --xunit junit-robot.xml tests/robot/")
+    c.run("robot --xunit robot-result.xml tests/robot/")
 
 @task
 def test_pytest(c):
-    c.run("pytest --junitxml=junit-pytest.xml")
+    c.run("pytest --junitxml=pytest-result.xml")
 
 @task(pre=[test_pytest, test_robot])
 def test_all(c):
