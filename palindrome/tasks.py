21cd4f8f81b8b16cb6b669c6d044459072ba91e2
thendarion <16006640+thendarion@users.noreply.github.com>
2025-05-15 07:34:33 +0200

    give reports a more natural file name
    Signed-off-by: thendarion <16006640+thendarion@users.noreply.github.com>

--- palindrome/tasks.py
+++ palindrome/tasks.py
@@ -3,11 +3,11 @@ from invoke import task
 
 @task
 def test_robot(c):
-    c.run("robot --xunit robot-result.xml tests/robot/")
+    c.run("robot --xunit robot-report.xml tests/robot/")
 
 @task
 def test_pytest(c):
-    c.run("pytest --junitxml=pytest-result.xml")
+    c.run("pytest --junitxml=pytest-report.xml")
 
 @task(pre=[test_pytest, test_robot])
 def test_all(c):
